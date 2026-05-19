6c3fcb7187e82252ca4882beeedb592f30eba650
Hamid <ayazi.hamidr@gmail.com>
2026-05-19 16:11:03 +0330

    updated OrderPage, added search and filters inputs

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -1,14 +1,49 @@
 import OrderTable from "../components/OrderTable";
-import { mockOrders } from "../data/mockOrders";
+import SearchInput from "../components/SearchInput";
+import { useOrders } from "../hooks/useOrders";
+import StatusFilter from "../components/StatusFilter";
+import SortSelect from "../components/SortSelect";
 
 export default function OrdersPage() {
-  return (
-    <main className="p-6">
-      <h1 className="mb-6 text-2xl font-bold">
-        مدیریت سفارش‌ها
-      </h1>
+  const { orders, filters, setFilters } = useOrders();
 
-      <OrderTable orders={mockOrders} />
+  return (
+    <main className="p-6 max-w-6xl mx-auto">
+      <h1 className="mb-6 text-2xl font-bold">مدیریت سفارش‌ها</h1>
+
+      {/* seach field */}
+      <div className="mb-4 flex flex-col gap-3 md:flex-row">
+        <SearchInput
+          value={filters.search}
+          onChange={(value) =>
+            setFilters({
+              search: value,
+              page: 1,
+            })
+          }
+        />
+
+        <StatusFilter
+          value={filters.status}
+          onChange={(value) =>
+            setFilters({
+              status: value,
+              page: 1,
+            })
+          }
+        />
+
+        <SortSelect
+          value={filters.sort}
+          onChange={(value) =>
+            setFilters({
+              sort: value,
+            })
+          }
+        />
+      </div>
+
+      <OrderTable orders={orders} />
     </main>
   );
 }
